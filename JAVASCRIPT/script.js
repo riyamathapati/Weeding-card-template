b817184f2c07157e926e1b88a78c38c99766bbd2
Riya Mathapati <mathapatiriya@gmail.com>
2023-10-26 14:30:51 +0530

    initial commit

---
+++ JAVASCRIPT/script.js
@@ -0,0 +1,34 @@
+
+let forimg2=document.querySelector("#p2");
+let foricon=document.querySelector("#forjsicon");
+let foricon1=document.querySelector("#forjsicon1");
+let fimg=document.querySelector("#p1");
+ function forimg(){
+   foricon.style.transform=("translate(-50%,-50%) scale(1)")
+   foricon.style.opacity=0.8
+ }
+ function forimgout(){
+   foricon.style.transform=("translate(-50%,-50%) scale(0)")
+   foricon.style.opacity=0
+
+ }
+ function forimg1(){
+   foricon1.style.transform=("translate(-50%,-50%) scale(1)")
+   foricon1.style.opacity=0.8
+ }
+ function forimgout2(){
+   foricon1.style.transform=("translate(-50%,-50%) scale(0)")
+   foricon1.style.opacity=0
+
+ }
+ function forshow(){
+  document.querySelector("#fordisplay").style.display="block";
+
+ }
+ function forhide(){
+  document.querySelector("#fordisplay").style.display="none";
+
+ }
+ 
+
+ 
